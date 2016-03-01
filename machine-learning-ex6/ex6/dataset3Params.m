c40fb517016a44c049945448806dbc26ba3e0f9e
Vinit Samel <vinitsamel@gmail.com>
2016-02-29 20:32:54 -0800

    finished

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,7 +8,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
+C = 30;
 sigma = 0.3;
 
 % ====================== YOUR CODE HERE ======================
@@ -23,12 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+options = [0.01 0.03 0.1 0.3 1 3 10 30];
+error_min = Inf;
 
+for COption = options
+    for SigmaOption = options
+        % Train the SVM
+        model= svmTrain(X, y, COption, @(x1, x2) gaussianKernel(x1, x2, SigmaOption));
+        predictions = svmPredict(model, Xval);
+        evalMean = mean(double(predictions ~= yval));
+        if (evalMean < error_min)
+            error_min = evalMean;
+            C = COption;
+            sigma = SigmaOption;
+        end
+    end
+end
 
-
-
-
-
+fprintf('C, sigma = [%f %f] with prediction error = %f\n', C, sigma, error_min);
 % =========================================================================
 
 end
--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,15 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
-
-
+    for idx = 1:length(vocabList)
+        if (1 == strcmp(vocabList{idx}, str))
+            word_indices = [word_indices ; idx];
+            break;
+        end
+    end         
     % =============================================================
 
 
